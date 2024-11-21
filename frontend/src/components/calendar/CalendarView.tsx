9aa63fe9124f9fce1a5b98b7261cf31180da9a5d
Dumitru <dima50066@gmail.com>
2024-11-21 14:28:28 +0100

    Work with calendar

--- frontend/src/components/calendar/CalendarView.tsx
+++ frontend/src/components/calendar/CalendarView.tsx
@@ -4,6 +4,15 @@ import { selectCalendarEntries } from "../../redux/calendar/selectors";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
+import "./CalendarView.css";
+import "moment/locale/uk";
+
+moment.updateLocale("uk", {
+  week: {
+    dow: 1,
+    doy: 4,
+  },
+});
 
 const localizer = momentLocalizer(moment);
 
@@ -12,32 +21,17 @@ const CalendarView: React.FC<{ onDayClick: (date: Date) => void }> = ({
 }) => {
   const calendarEntries = useSelector(selectCalendarEntries);
 
-  const events = calendarEntries.map((entry) => ({
-    title: entry.isWorkday ? "Working Day" : "Off Day",
-    start: new Date(entry.date),
-    end: new Date(entry.date),
-    isWorkday: entry.isWorkday,
-  }));
-
   const handleSlotSelect = ({ start }: { start: Date }) => {
     onDayClick(start);
   };
 
-  const getEventStyle = (event: any) => {
-    const backgroundColor = event.isWorkday ? "#4caf50" : "#ff5722";
-    return {
-      style: {
-        backgroundColor,
-        color: "#fff",
-        borderRadius: "50%",
-        padding: "4px 8px",
-        textAlign: "center" as React.CSSProperties["textAlign"],
-        boxShadow: "0px 3px 6px rgba(0, 0, 0, 0.1)",
-      },
-    };
-  };
-
   const getDayStyle = (date: Date) => {
+    const today = new Date();
+    const isToday =
+      today.getDate() === date.getDate() &&
+      today.getMonth() === date.getMonth() &&
+      today.getFullYear() === date.getFullYear();
+
     const entry = calendarEntries.find((entry) => {
       const entryDate = new Date(entry.date);
       return (
@@ -47,14 +41,18 @@ const CalendarView: React.FC<{ onDayClick: (date: Date) => void }> = ({
       );
     });
 
-    if (entry) {
+    if (isToday && entry) {
       return {
-        style: {
-          backgroundColor: entry.isWorkday ? "#d9fdd3" : "#fde8e8",
-          border: entry.isWorkday ? "1px solid #4caf50" : "1px solid #ff5722",
-          borderRadius: "4px",
-        },
+        className: entry.isWorkday ? "today-workday" : "today-holiday",
       };
+    } else if (entry) {
+      return {
+        className: entry.isWorkday ? "workday" : "holiday",
+      };
+    }
+
+    if (isToday) {
+      return { className: "today-neutral" };
     }
 
     return {};
@@ -64,14 +62,13 @@ const CalendarView: React.FC<{ onDayClick: (date: Date) => void }> = ({
     <div className="bg-white p-6 rounded-lg shadow-xl">
       <Calendar
         localizer={localizer}
-        events={events}
+        events={[]} // Відсутні події
         startAccessor="start"
         endAccessor="end"
         selectable
         onSelectSlot={handleSlotSelect}
         style={{ height: "600px" }}
         views={["month"]}
-        eventPropGetter={getEventStyle}
         dayPropGetter={getDayStyle}
         className="custom-calendar"
       />
