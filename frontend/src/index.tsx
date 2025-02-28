a19e590b3724697496db300fff65ca525752998c
Dumitru <dima50066@gmail.com>
2025-02-28 16:55:51 +0100

    add persistor

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,13 +1,17 @@
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import {store} from './redux/store';
-import App from './App';
-import './index.css';
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { store, persistor } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import App from "./App";
+import "./index.css";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
 root.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -9,20 +9,45 @@ import calendarReducer from "./calendar/slice";
 import documentsReducer from "./documents/slice";
 import coordinatorReducer from "./coordinator/slice";
 import rankingReducer from "./ranking/slice";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+import { combineReducers } from "redux";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: [
+    "auth",
+    "profile",
+    "calendar",
+    "departments",
+    "ranking",
+    "employees",
+    "documents",
+    "salary",
+    "productivity",
+  ],
+};
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+  profile: profileReducer,
+  productivity: productivityReducer,
+  departments: departmentsReducer,
+  salary: salaryReducer,
+  calendar: calendarReducer,
+  documents: documentsReducer,
+  employees: coordinatorReducer,
+  ranking: rankingReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    profile: profileReducer,
-    productivity: productivityReducer,
-    departments: departmentsReducer,
-    salary: salaryReducer,
-    calendar: calendarReducer,
-    documents: documentsReducer,
-    employees: coordinatorReducer,
-    ranking: rankingReducer,
-  },
+  reducer: persistedReducer,
 });
+
+export const persistor = persistStore(store);
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
